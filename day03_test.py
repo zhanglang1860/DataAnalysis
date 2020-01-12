69cf5fa3addc4b330ecf9340c72ccd97d83d8555
zhanglang1860 <zhanglang1860@users.noreply.github.com>
2020-01-12 22:24:30 +0800

    day03

--- day03_test.py
+++ day03_test.py
@@ -9,8 +9,7 @@ chart_studio.tools.set_credentials_file(
     api_key='MxJLQuDBGkAVEZI7f2J0'
 )
 # df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
-df = pd.read_csv(r'D:\Python数据分析教程\day03\code\youtube_video_data\GB_video_data_numbers.csv')
-print(df.head(5))
+df1 = pd.read_csv(r'D:\Python数据分析教程\day03\code\youtube_video_data\GBvideos.csv')
 
 # import plotly.express as px
 #
@@ -22,26 +21,37 @@ print(df.head(5))
 
 
 import plotly.graph_objects as go
-df = df[df['comment_total'] < 5000]
+
+# df = df[[df['likes'] < 10000] and [df['category_id'] < 5]]
+df = df1[(df1['likes'] < 10000) & (df1['category_id'] > 20)]
+print(df.head(5))
 fig = go.Figure()
-fig.add_trace(go.Histogram(
-    x=df['comment_total'],
-    histnorm='percent',
-    name='comment_total',
+fig.add_trace(go.Scatter(
+    x=df['likes'],
+    y=df['dislikes'],
+    name='likes-dislikes',
+    # marker={'color': 'red',
+    #         # 'symbol': 104,
+    #         # 'size': 10
+    #         },
+    mode='markers',
     # xbins=dict(
     #     start=-3.0,
     #     end=4,
     #     size=0.5
     # ),
-    marker_color='#330C73',
-    opacity=0.75
+    marker={'color': df['category_id']}
+    # fillcolor='#330C73',
+    # opacity=0.75
 ))
 fig.update_layout(
-    title_text='Sampled Results', # title of plot
-    xaxis_title_text='Value', # xaxis label
-    yaxis_title_text='Count', # yaxis label
-    bargap=0.2, # gap between bars of adjacent location coordinates
-    bargroupgap=0.1 # gap between bars of the same location coordinates
+    title_text='Sampled Results',  # title of plot
+    xaxis_title_text='Value',  # xaxis label
+    yaxis_title_text='Count',  # yaxis label
+    bargap=0.2,  # gap between bars of adjacent location coordinates
+    bargroupgap=0.1  # gap between bars of the same location coordinates
 )
 
 fig.show()
+
+# py.plot(fig, filename='pandas-multiple-scatter')
